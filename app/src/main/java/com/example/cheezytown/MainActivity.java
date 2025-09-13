65b3a3a867438980b92077c2b7b3678ce5353f93
Nati <115600522+Natimu@users.noreply.github.com>
2025-09-13 04:09:10 -0600

    fixes sides naming and added italian language

--- app/src/main/java/com/example/cheezytown/MainActivity.java
+++ app/src/main/java/com/example/cheezytown/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.cheezytown;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button clearButton = findViewById(R.id.clearButton);
+        Button checkOut = findViewById(R.id.Checkout);
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clear button clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        checkOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Checkout button clicked!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
